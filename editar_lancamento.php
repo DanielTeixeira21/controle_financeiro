4738b3763cc358bc465c073ea7a8746c17f9b6d6
Your Name <danielteixeira21@gmail.com>
2024-03-20 22:52:56 -0300

    llll

---
+++ editar_lancamento.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+      <label>Descrição</label>  
+      <input type="text" name="Descrição"/><br></br>
+      <label>valor</label>
+      <input Type="text" name="valor"/><br></br>
+      <label>Tipo</label>
+      <select name="tipo">
+        <option value="Entrada">Entrada</option>
+        <option value= "Saida">Saida</option>
+</select>
+<br></br>
+<label>Data</label>
+<input type="text" name="data"/><br></br>
+<label>Fixa</label>
+<select name="Fixa">
+    <option value="nao">Nao</option>
+    <option value= "sim">sim</option>
+</select>
+<br></br>
+<input type="submit" name="submit" value"Atualizar lancamento">
+</form>
+</body>
+</html>
